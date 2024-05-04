b53f5fc44d6dcf20d54fbe9f69e3286a5d5f4932
Avas <avas.bajracharya@renegadeinsurance.com>
2024-05-04 18:53:41 +0545

    feat: added text input

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import HeroText from "./components/HeroTextGroup/HeroTextGroup";
+import Input from "./components/Input";
 
 function App() {
   return (
-    <main className="w-screen h-screen flex justify-center items-center">
+    <main className="w-screen h-screen flex flex-col gap-12 justify-center items-center bg-background text-white">
       <HeroText firstName="Breaking" lastName="Bad" />
+      <section className="flex gap-4">
+        <Input label="First Name" />
+        <Input label="Last Name" />
+      </section>
     </main>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      background: "#282C33",
+      primary: "#036527",
+      gray: "#ABB2BF",
+      white: "#FFFFFF",
+      black: "#000000",
+    },
     extend: {},
   },
   plugins: [require("@tailwindcss/typography")],
